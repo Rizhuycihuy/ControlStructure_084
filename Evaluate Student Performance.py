4b8e9f187e6914f44a1f1dadd9671944a8d17562
Rizhuycihuy <rizki003131@gmail.com>
2026-09-22 10:47:25 +0700

    evaluasi nilai siswa

---
+++ Evaluate Student Performance.py
@@ -0,0 +1,12 @@
+Nilai = float(input("Enter student percentage: "))
+
+if Nilai >= 90:
+    print("Excellent performance")
+elif Nilai >= 80:
+    print("Very Good performance")
+elif Nilai >= 70:
+    print("Good performance")
+elif Nilai >= 60:
+    print("Average performance")
+else:
+    print("Below average performance")
