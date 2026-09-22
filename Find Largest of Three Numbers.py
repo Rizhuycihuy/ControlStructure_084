c259c470cf76ed51743c440126961365761b4bf1
Rizhuycihuy <rizki003131@gmail.com>
2026-09-22 11:08:01 +0700

    menambahkan situasi ketika ada nomor ganda

--- Find Largest of Three Numbers.py
+++ Find Largest of Three Numbers.py
@@ -2,11 +2,14 @@ a = int(input("Masukkan Angka Pertama : "))
 b = int(input("Masukkan Angka kedua : "))
 c = int(input("Masukkan Angka Ketiga : "))
 
-if a >= b and a >= c:
+if a > b and a > c:
     largest = a
-elif b >= a and b >= c:
+    print("Angka Terbesar Adalah:", largest)
+elif b > a and b > c:
     largest = b
-else:
+    print("Angka Terbesar Adalah:", largest)
+elif c > a and c > b:
     largest = c
-
-print("Angka Terbesar Adalah:", largest)
+    print("Angka Terbesar Adalah:", largest)
+else:
+    print("tidak ada angka terbesar")
